770695245276cd41b6edd13d3a29d5a5dcbdaaff
Xfarya <farya243@gmail.com>
2022-03-31 14:58:07 +0100

    added product schema

---
+++ src/model/Product.ts
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    name: { 
+        type: String,
+        required: true
+    },
+    description: { 
+        type: String,
+        required: true
+    },
+    price: { 
+        type: Number,
+        required: true
+    },
+    stock: { 
+        type: Number, 
+        required: true
+    },
+    category: { 
+        type: String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
+
