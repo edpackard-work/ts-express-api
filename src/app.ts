79dee060eb2c57130488eb7240397448398c5aa7
Ed Packard <edward.packard@kinandcarta.com>
2022-03-31 11:28:35 +0100

    Add connection to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express, {Application} from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import DemoRouter from './router/DemoRouter';
 
 const app:Application = express();
 
+dotenv.config()
+const dbConnectionString = process.env.MONGODB_URI as string;
+
 const demoRouter = new DemoRouter;
+
+(async () => {
+    try {
+        await mongoose.connect(dbConnectionString);
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        let message
+        if (error instanceof Error) message = error.message
+        else message = String(error);
+        console.log("Could not connect to Database: " + message)
+    }
+})();
 
 app.use(express.json());
 
