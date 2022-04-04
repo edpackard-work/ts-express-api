f1a70423caf78ca7dd3acf76682571e94a338fa1
Xfarya <farya243@gmail.com>
2022-04-04 09:58:12 +0100

    added formatting

--- src/__test__/ProductController.spec.ts
+++ src/__test__/ProductController.spec.ts
@@ -2,48 +2,51 @@ import { app } from '../app'
 import { productModel } from '../model/Product'
 
 import * as db from './setup/db'
-import request from "supertest";
+import request from 'supertest'
 
 const productData = {
-  name: "Pen",
+  name: 'Pen',
   description: "it's a pen",
   stock: 7,
   price: 11,
-  category: "stationery",
-};
+  category: 'stationery',
+}
 
 beforeAll(async () => {
-  await db.connect();
-});
+  await db.connect()
+})
 
 afterEach(async () => {
-  await db.clearDatabase();
-});
+  await db.clearDatabase()
+})
 
 afterAll(async () => {
-  await db.closeDatabase();
-});
+  await db.closeDatabase()
+})
 
 describe('test for /:id GET route', () => {
-
   it('gets a product with a valid id', async () => {
-    const validProduct = new productModel(productData);
-    const savedProduct = await validProduct.save();
+    const validProduct = new productModel(productData)
+    const savedProduct = await validProduct.save()
 
-    const response = await request(app).get(`/api/v1/products/${savedProduct._id}`)
-    
-    expect(response.statusCode).toEqual(200);
-    expect(response.body.name).toEqual("Pen");
-    expect(response.body.description).toEqual("it's a pen");
-    expect(response.body.stock).toEqual(7);
-    expect(response.body.price).toEqual(11);
-    expect(response.body.category).toEqual("stationery");
-    })
+    const response = await request(app).get(
+      `/api/v1/products/${savedProduct._id}`
+    )
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.name).toEqual('Pen')
+    expect(response.body.description).toEqual("it's a pen")
+    expect(response.body.stock).toEqual(7)
+    expect(response.body.price).toEqual(11)
+    expect(response.body.category).toEqual('stationery')
+  })
 
   it('returns correct status code and message for invalid input', async () => {
-    const response = await request(app).get("/api/v1/products/35354");
+    const response = await request(app).get('/api/v1/products/35354')
 
-    expect(response.statusCode).toEqual(404);
-    expect(response.text).toEqual("Unable to find matching document with id: 35354");
+    expect(response.statusCode).toEqual(404)
+    expect(response.text).toEqual(
+      'Unable to find matching document with id: 35354'
+    )
   })
 })
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, {Application} from 'express';
-import DemoRouter from './router/DemoRouter';
-import ProductRouter from './router/ProductRouter';
+import express, { Application } from 'express'
+import DemoRouter from './router/DemoRouter'
+import ProductRouter from './router/ProductRouter'
 
 export const app: Application = express()
 
-const demoRouter = new DemoRouter();
-const productRouter = new ProductRouter;
+const demoRouter = new DemoRouter()
+const productRouter = new ProductRouter()
 
 app.use(express.json())
 
-app.use('/demo', demoRouter.getRouter());
-app.use('/api/v1/products', productRouter.getRouter());
+app.use('/demo', demoRouter.getRouter())
+app.use('/api/v1/products', productRouter.getRouter())
 
-export default app;
+export default app
