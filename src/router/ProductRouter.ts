03fe30e8b2da8e00d9882cf0226b722bbd94dcda
Xfarya <farya243@gmail.com>
2022-04-04 16:40:51 +0100

    added client to router file

--- src/router/ProductRouter.ts
+++ src/router/ProductRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import ProductController from '../controller/ProductController'
-import { client as elasticClient } from '../elastic';
+import { client as elasticClient } from '../elastic'
 
 class ProductRouter {
   private router: Router
@@ -18,26 +18,26 @@ class ProductRouter {
     })
 
     this.router.get('/products', (req, res) => {
-      let query: any = { 
+      let query: any = {
         index: 'products',
       }
-      elasticClient.search(query)
-      .then((resp: { hits: { hits: any } })=>{
-        console.log(query);
-        return res.status(200).json({
-          products: resp.hits.hits
-        });
-      })
-      .catch((err: any)=>{
-        console.log(err);
-        return res.status(500).json({
-          msg: 'Error',
-          err
-        });
-      });
-  });
-  };
-
+      elasticClient
+        .search(query)
+        .then((resp: { hits: { hits: any } }) => {
+          console.log(query)
+          return res.status(200).json({
+            products: resp.hits.hits,
+          })
+        })
+        .catch((err: any) => {
+          console.log(err)
+          return res.status(500).json({
+            msg: 'Error',
+            err,
+          })
+        })
+    })
+  }
 
   public getRouter() {
     return this.router
