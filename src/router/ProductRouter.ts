eefa46f9957ab021a4a6bd8303d11347879f35c9
Xfarya <farya243@gmail.com>
2022-04-04 16:26:02 +0100

    added GET endpoint for elastic search

--- src/router/ProductRouter.ts
+++ src/router/ProductRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import ProductController from '../controller/ProductController'
+import { client as elasticClient } from '../elastic';
 
 class ProductRouter {
   private router: Router
@@ -12,10 +13,31 @@ class ProductRouter {
   }
 
   private setUpRouter() {
-    this.router.get('/:id', (req, res) => {
+    this.router.get('/products/:id', (req, res) => {
       this.controller.getProductById(req, res)
     })
-  }
+
+    this.router.get('/products', (req, res) => {
+      let query: any = { 
+        index: 'products',
+      }
+      elasticClient.search(query)
+      .then((resp: { hits: { hits: any } })=>{
+        console.log(query);
+        return res.status(200).json({
+          products: resp.hits.hits
+        });
+      })
+      .catch((err: any)=>{
+        console.log(err);
+        return res.status(500).json({
+          msg: 'Error',
+          err
+        });
+      });
+  });
+  };
+
 
   public getRouter() {
     return this.router
