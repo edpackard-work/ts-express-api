4be7cdf8a0111e8c25b43c8ae6e0e3fe6965f240
Xfarya <farya243@gmail.com>
2022-04-07 17:33:18 +0100

    returned the s

--- src/controller/ProductController/buildQuery.ts
+++ src/controller/ProductController/buildQuery.ts
@@ -44,7 +44,7 @@ const makeEmptyQuery = (): any => ({
 })
 
 const isLimitValid = (query: any): any => {
-  return query <= 10000 && query != NaN;
+  return query <= 10000 && query != NaN
 }
 
 export const buildQuery = (req: Request) => {
